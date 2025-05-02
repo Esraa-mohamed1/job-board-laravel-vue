636ce25eab1f9208151cf69e1a2dc80b4fb75ca6
Notebook <omarokstar2001@gmail.com>
2025-05-02 15:22:20 +0300

    Folders Structure

---
+++ job-board/src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout">
+ 
+      <main class="main-content">
+        <router-view />
+        <h1>Welcome </h1>
+        <img src="https://m.gomhuriaonline.com/Upload/News/16-10-2019_13_45_29_GomhuriaOnline_1571226329.jpg" alt="Logo" class="logo" />
+      </main>
+    </div>
+  </template>
+  
